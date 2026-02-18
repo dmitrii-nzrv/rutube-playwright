c32eaa9b457001bb89f3f7d04c624601990c1efe
Dmitrii <dmitrii.nazarov7703@gmail.com>
2026-02-19 00:35:42 +0300

    add main page tests

--- tests/specs/unauthorized/mainPage.spec.ts
+++ tests/specs/unauthorized/mainPage.spec.ts
@@ -1,7 +1,23 @@
 import test from '@playwright/test';
 import { MainPage } from '../../pages/mainPage';
 
-test('Открытие главной страницы', async ({ page }) => {
+test('Проверка доступности элементов хедера', async ({ page }) => {
   const mainPage = new MainPage(page);
   mainPage.open();
+  await mainPage.closeCookiesAlert();
+  mainPage.headerHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов табов категорий', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  mainPage.open();
+  await mainPage.closeCookiesAlert();
+  mainPage.categoriesTabsHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов бокового меню', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  mainPage.open();
+  await mainPage.closeCookiesAlert();
+  mainPage.menuHasCorrectAriaSnapshot();
 });
